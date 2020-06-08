479f1ca7bd69537a64461fe7b1b24954bbc11d86
Gunslinger <gunslinger-ovo@qq.com>
2020-06-08 23:57:40 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM golang:1.14.4-alpine
 WORKDIR /go/src/app
 COPY . .
 
+RUN apk add --no-cache git
 RUN go get -d -v ./...
 RUN go install -v ./...
 
--- goproxy.go
+++ goproxy.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/goproxy/goproxy"
 )
 
 func main() {
-	http.ListenAndServe("0.0.0.0:8080", goproxy.New())
+	g := goproxy.New()
+	g.GoBinEnv = append(
+		os.Environ(),
+		"GOPROXY=https://goproxy.cn,direct",
+	)
+	g.ProxiedSUMDBs = []string{"sum.golang.org https://sum.golang.google.cn"}
+	http.ListenAndServe("0.0.0.0:8080", g)
 }
