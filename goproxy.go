0830d82bcc2b779f492218b193459cd1995d2254
Gunslinger <gunslinger-ovo@qq.com>
2020-07-28 23:31:54 +0800

    Update goproxy

--- goproxy.go
+++ goproxy.go
@@ -5,15 +5,18 @@ import (
 	"os"
 
 	"github.com/goproxy/goproxy"
+	"github.com/goproxy/goproxy/cacher"
 )
 
 func main() {
 	g := goproxy.New()
 	g.GoBinEnv = append(
 		os.Environ(),
-		"GOPROXY=https://goproxy.cn,direct",
-		"GO111MODULE=on",
+		"GOPROXY=https://goproxy.cn,direct", // 使用 goproxy.cn 作为上游代理
 	)
-	g.ProxiedSUMDBs = []string{"sum.golang.org https://sum.golang.google.cn"}
+	g.ProxiedSUMDBs = []string{"sum.golang.org https://goproxy.cn/sumdb/sum.golang.org"} // 代理默认的校验和数据库
+	g.Cacher = &cacher.Disk{
+		Root: "/cache",
+	}
 	http.ListenAndServe("0.0.0.0:8080", g)
 }
